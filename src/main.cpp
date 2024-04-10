e32d275ac13ae16ef86f0888e886258659a01dbf
Allan Burleson <allanburleson@users.noreply.github.com>
2024-04-10 17:53:28 -0500

    Use semaphore for sending

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,10 @@ uuids UUID_generator;
 Bluetooth<uuids> bluetooth;
 volatile SemaphoreHandle_t disconnect_semaphore;
 volatile SemaphoreHandle_t scan_semaphore;
+volatile SemaphoreHandle_t send_semaphore;
 volatile int8_t DISCON_ISR = 0;
 volatile int8_t SCAN_ISR = 0;
+volatile int8_t SEND_ISR = 0;
 portMUX_TYPE isr_mux = portMUX_INITIALIZER_UNLOCKED;
 
 void ARDUINO_ISR_ATTR set_semaphore()
@@ -35,6 +37,15 @@ void ARDUINO_ISR_ATTR set_semaphore()
     {
         SCAN_ISR++;
     }
+
+    if (SEND_ISR == 2)
+    {
+        xSemaphoreGiveFromISR(send_semaphore, NULL);
+    }
+    else
+    {
+        SEND_ISR++;
+    }
     taskEXIT_CRITICAL_ISR(&isr_mux);
 }
 
@@ -50,27 +61,30 @@ void setup()
 
     disconnect_semaphore = xSemaphoreCreateBinary();
     scan_semaphore = xSemaphoreCreateBinary();
+    send_semaphore = xSemaphoreCreateBinary();
     setup_timer(set_semaphore, 250, 80);
 }
 
 void loop()
 {
-    if (bluetooth.clientIsConnected()) // only worry about finding sensors after repeater is connected
+    if (xSemaphoreTake(send_semaphore, 0) == pdTRUE)
     {
-        digitalWrite(LED_BUILTIN, HIGH);
-        if (received_packets.size() > 0)
+        printf("Send semaphore ready...\n");
+        if (bluetooth.clientIsConnected()) // only worry about finding sensors after repeater is connected
         {
-            // admittedly cursed
-            bluetooth.callback_class->setData(received_packets.front());
-            received_packets.pop();
-            bluetooth.sendData();
+            digitalWrite(LED_BUILTIN, HIGH);
+            if (received_packets.size() > 0)
+            {
+                bluetooth.callback_class->setData(received_packets.front());
+                received_packets.pop();
+                bluetooth.sendData();
+            }
+            bluetooth.tryConnectToServer();
         }
-        bluetooth.tryConnectToServer();
-        // ALWAYS scan, because devices could become available at any time
-    }
-    else
-    {
-        digitalWrite(LED_BUILTIN, LOW);
+        else
+        {
+            digitalWrite(LED_BUILTIN, LOW);
+        }
     }
     if (xSemaphoreTake(scan_semaphore, 0) == pdTRUE && bluetooth.clientIsConnected())
     {
